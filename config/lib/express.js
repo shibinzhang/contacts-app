d5f8976b8167dc650aa79aeb7720d8dfe4214a39
shibinzhang <zhangshibin@yahoo.com>
2016-07-14 13:22:43 -0700

    update

--- config/lib/express.js
+++ config/lib/express.js
@@ -6,6 +6,18 @@ var express = require ('express');//3rd party library
 
 module.exports.init = function (){
     var app = express ();
+    //body parser middleware integration
+    this.initBodyParser (app);
     
     return app;
-}
+};
+
+var bodyParser = require ('body-parser');
+module.exports.initBodyParser = function (app) {
+// parse application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: false }))
+
+    // parse application/json
+    app.use(bodyParser.json())
+
+};
